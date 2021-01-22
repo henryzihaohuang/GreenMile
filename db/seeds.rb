be7d75823c821df6feb091685fdd43e5b5ac24fd
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-01-22 16:05:28 -0600

    seed demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-dolly = User.create!({email: "dollyparton@gmail.com", password: "password"}
+dolly = User.create!({email: "dollyparton@gmail.com", password: "password"})
