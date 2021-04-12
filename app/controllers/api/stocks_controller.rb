82224df5795f24f5ee69d116983f2396c8da4a1a
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-04-11 21:52:36 -0500

    Fix build error

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -1,15 +1,15 @@
-class Api::StocksController < ApplicationController
+# class Api::StocksController < ApplicationController
 
-    def index
-        @stocks = Stock.all
-        render "api/stocks/index"
-    end
+#     def index
+#         @stocks = Stock.all
+#         render "api/stocks/index"
+#     end
 
-    def show
-        @show = Stock.find_by(id:params[:id])
+#     def show
+#         @show = Stock.find_by(id:params[:id])
 
-        @stock ? render "api/stocks/show" : render json: @stock.errors.full_messages, status:401
-    end
+#         @stock ? render "api/stocks/show" : render json: @stock.errors.full_messages, status:401
+#     end
 
     
-end
+# end
