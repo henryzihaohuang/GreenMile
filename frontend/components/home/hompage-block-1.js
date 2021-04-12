f35b36934225230d9643573043d38fccace7868c
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-04-11 22:09:00 -0500

    Change text size for block one

--- frontend/components/home/hompage-block-1.js
+++ frontend/components/home/hompage-block-1.js
@@ -15,7 +15,9 @@ class HomeBlockOne extends React.Component {
                             <video src="https://cdn.robinhood.com/assets/superbowl/superbowl.mp4" autoPlay controlsList="nodownload nofullscreen noremoteplayback" loop muted playsInline preload="auto" />
                         </div>
                         <div className="block-content-text">
-                            We are all investors. 
+                            <span className="block-content-header">
+                                We are all investors. 
+                            </span>
                             <br />
                             See the campaign <img></img>
                         </div>
